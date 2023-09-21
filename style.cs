f34676749356e6fe6137b67317986c1cc7711f6c
saurabh533 <124454892+saurabh533@users.noreply.github.com>
2023-09-21 17:40:08 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website</title>
+    <style>
+        /* Reset some default styles */
+        body, h1, h2, p {
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Style your header */
+        header {
+            background-image: url('https://www.inteliment.com/wp-content/uploads/2021/05/x37-The-Techniques-Team-and-Tools-for-Effective-Data-Science.jpg.pagespeed.ic.7bq9FotD1L.webp');
+            background-size: cover; /* Adjust to your needs */
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        /* Style your navigation */
+        nav ul {
+            background-image: url('path-to-your-navigation-image.jpg');
+            background-size: cover; /* Adjust to your needs */
+            background-repeat: no-repeat;
+            background-position: center center;
+            list-style-type: none;
+            background-color: #333;
+            text-align: center;
+            padding: 10px 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin: 0 15px;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+            color: #fff;
+        }
+
+        /* Style your main content */
+        main {
+            background-image: url('https://23g-sharedhosting-actregtech.s3.eu-west-1.amazonaws.com/app/uploads/2022/02/09094823/neual-network-2.jpg');
+            background-size: cover; /* Adjust to your needs */
+            background-repeat: no-repeat;
+            background-position: center center;
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        /* Style your footer */
+        footer {
+            background-image: url('https://www.analyticsinsight.net/wp-content/uploads/2022/10/Data-Science-Market-Size-is-Expected-to-Reach-US33.3-Billion-by-2025.jpg');
+            background-size: cover; /* Adjust to your needs */
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <!-- Header content goes here -->
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    
+    <main>
+        <!-- Main content goes here -->
+    </main>
+    
+    <footer>
+        <!-- Footer content goes here -->
+    </footer>
+</body>
+</html>
